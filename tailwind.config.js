1a6829783227101895cf9f8ce6c4a0227d4e4007
Abid <abid0hussain1733@gmail.com>
2023-05-19 10:03:02 +0530

    added config for custom width and height

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,13 @@ module.exports = {
         'very-dark-blue': '#090b1a',
         'dark-desaturated-blue': '#1b1938',
         'soft-violet': '#aa5cdb'
+      },
+      spacing: {
+        '320': '320px',
+        '375': '375px',
+        '400': '400px',
+        '570': '570px',
+        '1110': '1110px'
       }
     },
   },
